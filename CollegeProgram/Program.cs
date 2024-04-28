ab8e81a4aab07ccef36f380dadec9023b90846fa
Pravakar Rijal <pravakarrijal11@gmail.com>
2024-04-28 22:55:43 +0545

    Add CollegeProgram Project

---
+++ CollegeProgram/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+
+namespace CollegeProgram
+{ 
+    internal class Program
+    {
+        internal static void Main()
+        {
+            string ConsoleInput;
+            StreamWriter wr = new StreamWriter("C:\\Users\\USER\\Documents\\PRAVAKAR\\NET Centric\\file.txt");
+            Console.WriteLine("Enter the text to write on File: ");
+            ConsoleInput = Console.ReadLine();
+            wr.WriteLine(ConsoleInput);
+            wr.Close();
+
+            string FileOutput;
+            StreamReader rw = new StreamReader("C:\\Users\\USER\\Documents\\PRAVAKAR\\NET Centric\\file.txt");
+            FileOutput = rw.ReadLine();
+            Console.WriteLine("Output from file: \n" + FileOutput);
+        }
+    }
+}
